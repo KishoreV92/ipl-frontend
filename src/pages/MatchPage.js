53c6bc17ed9c3993be4ff3ace9f1fa630a103002
KishoreV92 <kishorevjn835@gmail.com>
2021-08-09 21:01:11 +0530

    1 file modified

--- src/pages/MatchPage.js
+++ src/pages/MatchPage.js
@@ -4,9 +4,27 @@ import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 
 export const MatchPage = () => {
+  const [matches, setMatches] = useState([{}]);
+  const { teamName, year } = useParams();
+
+  useEffect(() => {
+    const fetchMatches = async () => {
+      const data = await fetch(
+        `http://localhost:8080/team/${teamName}/matches?year=${year}`
+      ).then(res => {
+        return res.json();
+      });
+      setMatches(data);
+    };
+    fetchMatches();
+  }, []);
+
+  if (!matches) return null;
   return (
     <div className="MatchPage">
-      <h1>Match Page</h1>
+      {matches.map(match => (
+        <MatchDetailCard teamName={teamName} match={match} />
+      ))}
     </div>
   );
 };
