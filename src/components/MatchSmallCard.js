bc39575456879e46a1a83c3d9c714e97803e135e
KishoreV92 <kishorevjn835@gmail.com>
2021-08-10 09:49:08 +0530

    7 files modified, 2 added

--- src/components/MatchSmallCard.js
+++ src/components/MatchSmallCard.js
@@ -4,12 +4,18 @@ import { Link } from 'react-router-dom';
 export const MatchSmallCard = ({ teamName, match }) => {
   const otherTeam = match.team1 === teamName ? match.team2 : match.team1;
   const otherTeamRoute = `/teams/${otherTeam}`;
+  const isMatchWon = teamName === match.matchWinner;
   return (
-    <div className="MatchSmallCard">
+    <div
+      className={
+        isMatchWon ? 'MatchSmallCard won-card' : 'MatchSmallCard lost-card'
+      }
+    >
+      <span className="vs">vs</span>
       <h3>
-        vs <Link to={otherTeamRoute}>{otherTeam}</Link>
+        <Link to={otherTeamRoute}>{otherTeam}</Link>
       </h3>
-      <p>
+      <p className="match-result">
         {match.matchWinner} won by {match.resultMargin} {match.result}
       </p>
     </div>
